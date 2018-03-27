cb5932fe213faa59c3fd8690efde7ed4ea561dee
was <zxt_zel_npl@163.com>
2018-03-27 09:33:46 +0800

    关于IE8的兼容性移动走

--- mobile/javascript/ie8Compatibility.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-!function(){
-  if(typeof String.prototype.trim !== "function"){
-    String.prototype.trim = function () {
-      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
-    };
-  }
-}()
