c21fcee615bd76a64f47963b7886ca446828a4ea
was <zxt_zel_npl@163.com>
2018-03-29 10:40:13 +0800

    fontScale:agreementsignature and testdirection

--- mobile/javascript/selfFontScale.js
+++ mobile/javascript/selfFontScale.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (function ($) {
-  var SelfFontScale = (function () {
+  var SelfFontScale;
+  SelfFontScale = (function () {
     function SelfFontScale(element, options) {
       this.settings = $.extend(true, $.fn.SelfFontScale.defaults, options || {})
       this.$element = $(element)
@@ -10,40 +11,37 @@
 
     SelfFontScale.prototype = {
       init: function () {
-        console.log('init')
+        this.current = 0;
         this.texts = this.$element.find(this.settings.selectors.text);
         this.__render();
       },
 
       __render: function () {
-        console.log('__render')
-        this.addBtn = $("<div class='textAddBtn'>A+</div>").css({})
-        this.minusBtn = $("<div class='textMinusBtn'>A-</div>").css({})
+        this.addBtn = $("<div class='" + this.settings.selectors.addBtn + "'>A+</div>")
+        this.minusBtn = $("<div class='" + this.settings.selectors.minusBtn + "'>A-</div>")
 
-        this.btnBox = $("<div class='btnBox'></div>")
+        this.btnBox = $("<div class='" + this.settings.selectors.btnBox + "'></div>")
           .append(this.addBtn)
-          .append(this.minusBtn)
-          .css({})
+          .append(this.minusBtn);
 
 
-        this.addBtn.click(this.__add.bind(this))
-        this.minusBtn.click(this.__minus.bind(this))
+        this.addBtn.click(this.__add.bind(this));
+        this.minusBtn.click(this.__minus.bind(this));
         this.$element.append(this.btnBox)
       },
 
       __add: function () {
         var me = this;
         me.settings.current += me.settings.step;
-        if(me.settings.current>me.settings.max){
+        if (me.settings.current > me.settings.max) {
           me.settings.current = me.settings.max
+          return
         }
 
 
-        this.texts.css('fontSize',function(index,fontSize){
+        this.texts.css('fontSize', function (index, fontSize) {
           var __fontSize = parseInt(fontSize),
-            fontSize = __fontSize + me.settings.current;
-          console.log(me.settings.current)
-          console.log(fontSize)
+            fontSize = __fontSize + me.settings.step;
           return fontSize
 
         })
@@ -51,19 +49,23 @@
       },
 
       __minus: function () {
-        this.current -= this.step;
-        if(this.current<this.min){
-          this.current = this.min
+        var me = this
+        me.settings.current -= me.settings.step;
+        if (me.settings.current < me.settings.min) {
+          me.settings.current = me.settings.min
+          return
         }
 
-        this.texts.css('fontSize',function(preCss,nextCss){
-          console.log(preCss)
+        this.texts.css('fontSize', function (index, fontSize) {
+          var __fontSize = parseInt(fontSize),
+            fontSize = __fontSize - me.settings.step;
+          return fontSize
         })
       }
     }
 
     return SelfFontScale
-  })()
+  })();
 
   $.fn.SelfFontScale = function (options) {
     return this.each(function () {
@@ -77,18 +79,19 @@
 
   $.fn.SelfFontScale.defaults = {
     selectors: {
-      text: '.text',
-      addBtn: '.textAddBtn',
-      minusBtn: '.textMinusBtn',
-      btnBox: '.btnBox'
+      text: '.canScale',
+      addBtn: 'textAddBtn',
+      minusBtn: 'textMinusBtn',
+      btnBox: 'btnBox'
     },
     step: 2,
     max: 10,
     min: 0,
     current: 0
   }
-
-  $("body").SelfFontScale()
-
 })(jQuery)
+
+$(function(){
+  $("body").SelfFontScale()
+})
 
